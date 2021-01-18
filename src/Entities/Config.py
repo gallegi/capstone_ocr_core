e6ad1e9fc2d6754dc0f8540cbf502e1f9d57b662
BinhNA1 <sometimesocrazy@gmail.com>
2021-01-18 16:15:28 +0700

    Add deffault config file
    Signed-off-by: BinhNA1 <sometimesocrazy@gmail.com>

--- src/Entities/Config.py
+++ src/Entities/Config.py
@@ -1,29 +1,30 @@
-import string
 import os
+import string
+
 
 class Config:
     def __init__(self):
-        alphabet =[]
-        alphabet += sorted(list(set(string.digits.lower() + string.ascii_letters.lower() + '' + '-ọụạảãàáâậầấẩẫăắằặẳẵóòõỏôộổỗồốơờớợởỡéèẻẽêếềệểễúùủũưựữửừứíìịỉĩýỳỷỵỹđ:;,.')))
+        alphabet = []
+        alphabet += sorted(list(set(
+            string.digits.lower() + string.ascii_letters.lower() + '' + '-ọụạảãàáâậầấẩẫăắằặẳẵóòõỏôộổỗồốơờớợởỡéèẻẽêếềệểễúùủũưựữửừứíìịỉĩýỳỷỵỹđ:;,.')))
         alphabet += string.digits + string.ascii_lowercase
-        alphabet +=  '[],.)(?!/'
+        alphabet += '[],.)(?!/'
         alphabet = list(set(alphabet))
         alphabet = sorted(alphabet)
         alphabet = ''.join(alphabet)
         print('Prepared alphabet : {}'.format(alphabet))
 
-
         self.alphabet = alphabet
-        self.recognizer_weights_path ='weights/bill_recognizer_LabelmeDataset.h5'
+        self.recognizer_weights_path = 'weights/bill_recognizer_LabelmeDataset.h5'
 
         # doc_clf model and data path
         # self.doc_clf_data_source_path = "data_source/document_clf_db.csv"
         # self.doc_clf_data_source_path_original = "data_source/document_clf_db_bku.csv"
-        self.doc_clf_connection_string = 'Driver={SQL Server};'\
-                                          'Server=13.82.21.132;'\
-                                          'Database=VNPOST_Appointment_dev;'\
-                                          'UID=service;'\
-                                          'PWD=Service123;'
+        self.doc_clf_connection_string = 'Driver={SQL Server};' \
+                                         'Server=13.82.21.132;' \
+                                         'Database=VNPOST_Appointment_dev;' \
+                                         'UID=service;' \
+                                         'PWD=Service123;'
         self.doc_clf_model = "weights/doc_clf_model.pkl"
         self.doc_clf_ft_ext = "weights/doc_clf_ft_ext.pkl"
 
@@ -33,15 +34,16 @@ class Config:
         self.ner_engine_path = os.getcwd() + r'/weights/VnCoreNLP-1.1.1.jar'
         self.read_config()
 
-    def read_config(self,config_path = 'weights/config.txt'):
-        lines = open(config_path,encoding='utf-8').read().split('\n')
-        data= {}
+    def read_config(self, config_path='weights/config.txt'):
+        lines = open(config_path, encoding='utf-8').read().split('\n')
+        data = {}
         for line in lines:
             z = line.split('=')
-            if len(z)==2:
-                data[z[0]]=z[1]
-        self.port=  data['PORT']
+            if len(z) == 2:
+                data[z[0]] = z[1]
+        self.port = data['PORT']
         self.is_debug = int(data['DEBUG'])
+
 
 if __name__ == '__main__':
     config = Config()
